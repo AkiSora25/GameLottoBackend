dde84ef7d1a9e5922725891bf362fc7b6f3e7e20
My Name <myemail@example.com>
2025-01-20 15:13:06 +0100

    videogame 1

--- src/main/java/com/generation/gamelottobackend/model/entities/Videogame.java
+++ src/main/java/com/generation/gamelottobackend/model/entities/Videogame.java
@@ -1,4 +1,15 @@
 package com.generation.gamelottobackend.model.entities;
 
-public class Videogame {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Videogame extends BaseEntity
+{
+	private String name;
+	private String platform;
+	private String genres;
+	private int releaseYear;
+	private String developer;
+	private String soundtrack;
+
 }
