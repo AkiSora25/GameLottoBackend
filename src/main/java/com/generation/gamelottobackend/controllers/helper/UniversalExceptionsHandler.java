23a71f86744d988a75fc60c6b7993fc88a8fb852
AkiSora25 <marco3356@outlook.it>
2025-01-21 13:05:23 +0100

    videogame 5

--- src/main/java/com/generation/gamelottobackend/controllers/helper/UniversalExceptionsHandler.java
+++ src/main/java/com/generation/gamelottobackend/controllers/helper/UniversalExceptionsHandler.java
@@ -1,7 +1,10 @@
 package com.generation.gamelottobackend.controllers.helper;
 
 
+import com.generation.gamelottobackend.controllers.exceptions.CoverNotFoundException;
+import com.generation.gamelottobackend.controllers.exceptions.SoundtrackNotFoundException;
 import com.generation.gamelottobackend.controllers.exceptions.VideogameNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,4 +12,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class UniversalExceptionsHandler
 {
+    @ExceptionHandler(VideogameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleVideogameNotFound(VideogameNotFoundException e)
+    {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(CoverNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleCoverNotFound(CoverNotFoundException e)
+    {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(SoundtrackNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleSoundNotFound(SoundtrackNotFoundException e)
+    {
+        return e.getMessage();
+    }
 }
